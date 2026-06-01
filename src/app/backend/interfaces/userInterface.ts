83f7ccec21d5ba6ad60cabd1ca57b07747bd9ade
gouravrajak985 <gouravrajak985+github@gmail.com>
2026-06-02 01:39:49 +0530

    Added the user interface.

---
+++ src/app/backend/interfaces/userInterface.ts
@@ -0,0 +1,29 @@
+export enum UserRole {
+  BLOCKED = "blocked",
+  DELETED = "deleted",
+  STUDENT = "student",
+  MENTOR = "mentor",
+  ADMIN = "admin",
+  SUPER_ADMIN = "super_admin",
+}
+
+export interface IUser {
+    _id: string;
+    userName: string;
+    role:UserRole;
+    email:string;
+}
+export interface IParticipant {
+    participantName:string;
+    role:UserRole;
+}
+export const RoleHierarchy = {
+  [UserRole.BLOCKED]: -2,
+  [UserRole.DELETED]: -1,
+  [UserRole.STUDENT]: 1,
+  [UserRole.MENTOR]: 2,
+  [UserRole.ADMIN]: 3,
+  [UserRole.SUPER_ADMIN]: 4
+} as const;
+
+
